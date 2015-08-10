98768ba312d134a3e40abcbb51ec892e495ee6ce
Rob Shipley <robshipley@tcc-net.com>
2015-08-10 17:17:00 +0100

    Moved config settings to allow config merging to be cached

--- config/service/service.config.php
+++ config/service/service.config.php
@@ -37,6 +37,54 @@ return array(
             $orderService->setOrderLineMapper($sm->get('speckorder_orderLineMapper'));
             return $orderService;
         },
+        'speckorder_form_ordersearch' => function ($sm) {
+            $orderService = $sm->get('speckorder_service_orderservice');
+            $opts['tags'] = $orderService->getAllTags();
+            $form = new \SpeckOrder\Form\OrderSearch($opts);
+            return $form;
+        },
+        'speckorder_config' => function ($sm) {
+            $config = array(
+                'view_order_placeholders' => array(
+                    //relative to view directory
+                    'content_1' => '/speck-order/order-management/order/placeholder/content-1',
+                    'content_2' => '/speck-order/order-management/order/placeholder/content-2',
+                    'content_3' => '/speck-order/order-management/order/placeholder/content-3',
+                ),
+                'search_form_fieldset_partials' => array(
+                    //relative to view directory
+                    'filters' => '/speck-order/order-management/search/partial/search-filters',
+                    'text'    => '/speck-order/order-management/search/partial/search-text',
+                    'default' => '/speck-order/order-management/search/partial/search-fieldset',
+                ),
+                'order_actions' => array(
+                    'invoice' => array(
+                        'type'  => 'uri',
+                        'index' => 'invoice',
+                        'uri'   => '/manage-order/{order_id}/invoice',
+                        'label' => 'View Invoice',
+                        'title' => 'Invoice - Payment Due',
+                    ),
+                    'receipt' => array(
+                        'type'  => 'uri',
+                        'index' => 'receipt',
+                        'label' => 'View Receipt',
+                        'title' => 'Payment Not Received',
+                    ),
+                ),
+            );
+            $smConfig = $sm->get('Config');
+            if(isset($smConfig['speckorder'])) {
+                $config = \Zend\Stdlib\ArrayUtils::merge($config, $smConfig['speckorder']);
+            }
+            return $config;
+        },
+        'speckorder_form_ordertags' => function ($sm) {
+            $orderService = $sm->get('speckorder_service_orderservice');
+            $opts['tags'] = $orderService->getAllTags();
+            $form = new \SpeckOrder\Form\OrderTags($opts);
+            return $form;
+        },
         'speckorder_orderMapper' => function ($sm) {
             $hydrator = new ClassMethods();
             $hydrator->addStrategy('created', new DateTime());
